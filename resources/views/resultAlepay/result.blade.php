a55d029cfec6d14495eceff2fc0a8a28417d87c0
cuongdv <cuongdao2692@gmail.com>
2022-04-13 05:58:38 +0700

    sua code

--- resources/views/resultAlepay/result.blade.php
+++ resources/views/resultAlepay/result.blade.php
@@ -9,6 +9,10 @@ $utils = new AlepayUtils();
 ?>
 
 <link rel="stylesheet" type="text/css" href="https://cdn.tgdd.vn/mwgcart/vue-pro/css/desktop/cart-result.min.2ab80dc527e5d6fd453e.css">
+   <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
 
 <!doctype html>
 <html lang="en">
