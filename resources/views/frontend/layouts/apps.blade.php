6382ae25422189f125449de4418375b673225df2
cuongdv <cuongdao2692@gmail.com>
2022-04-20 04:47:38 +0700

    sua code

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -1885,7 +1885,7 @@
             
         </script>
 
-        <script src="{{ asset('js/lib/jquery-3.5.1.min.js') }}"></script>
+        <script src="{{ asset('js/lib/jquery.min.js') }}"></script>
         
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
 
