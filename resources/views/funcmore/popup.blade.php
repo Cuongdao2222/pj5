95d519dbaf35397301fdcc8b27f48626445ab5bf
cuongdv <cuongdao2692@gmail.com>
2022-05-10 09:40:31 +0700

    sua code

--- resources/views/funcmore/popup.blade.php
+++ resources/views/funcmore/popup.blade.php
@@ -214,7 +214,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Thay Ảnh nền</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Thay Meta trang Home</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
