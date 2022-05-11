0bd00106ed11452b2f133c97482b5e0154b3a9c4
cuongdv <cuongdao2692@gmail.com>
2022-05-11 15:36:19 +0700

    sua code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -459,7 +459,7 @@
                         
                         
                     </div>
-                    <a class="readmore-txt blue" href="{{ route('details', @$groups->link)  }}"><span>Xem tất cả <b>{{ @count(json_decode($data))}}</b> {{ @$groups->name }}</span></a>
+                    <a class="readmore-txt blue" href="{{ route('details', @$groups->link)  }}"><span>Xem tất cả {{ @$groups->name }}</span></a>
                 </div>
             </div>
         </div>
