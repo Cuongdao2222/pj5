bcd724a88fa036b552d562f36b8eb5211cab2578
Cuongdao <cuongdao2692@gmail.com>
2022-05-17 16:29:23 +0700

    update code

--- app/Http/Controllers/Frontend/indexController.php
+++ app/Http/Controllers/Frontend/indexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\banners;
+use Cache;
 
 
 
@@ -13,13 +14,22 @@ class indexController extends Controller
     public function index()
     {
 
-        $banners = banners::where('option', 0)->OrderBy('stt', 'asc')->where('active', 1)->get();
+        $banners = Cache::rememberForever('banners', function() {
+            return banners::where('option', 0)->OrderBy('stt', 'asc')->where('active', 1)->get();
+        });
 
-        $bannersRight = banners::where('option', 2)->OrderBy('stt', 'asc')->where('active', 1)->get();
+        $bannersRight = Cache::rememberForever('bannersRight', function() {
+            return  banners::where('option', 2)->OrderBy('stt', 'asc')->where('active', 1)->get();
+        });
 
-        $bannerUnderSlider = banners::where('option', 3)->OrderBy('stt', 'asc')->where('active', 1)->get();
+        $bannerUnderSlider = Cache::rememberForever('bannerUnderSlider', function() {
+            return  banners::where('option', 3)->OrderBy('stt', 'asc')->where('active', 1)->get();
+        });
 
-        $bannerUnderSale   = banners::where('option', 5)->OrderBy('stt', 'asc')->take(1)->get()->toArray();
+        $bannerUnderSale  = Cache::rememberForever('bannerUnderSale', function() {
+            return  banners::where('option', 5)->OrderBy('stt', 'asc')->take(1)->get()->toArray();
+        });
+
         return view('frontend.index', compact('banners', 'bannersRight', 'bannerUnderSlider', 'bannerUnderSale'));
     }
    
