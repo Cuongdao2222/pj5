e1adcdffd584be7bd86e4ffddbc970af01a2748f
cuongdv <cuongdao2692@gmail.com>
2022-05-24 18:56:52 +0700

    update

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -4,7 +4,7 @@
     @if(isset($arr_number))
     @foreach($arr_number as $numbers)
         <sitemap>
-            <loc>https://dienmaynguoiviet.vn/sitemap_pc{{trim($numbers)}}</loc>
+            <loc>https://dienmaynguoiviet.vn/sitemap_pc{{trim($numbers)}}.xml</loc>
             <lastmod>{{ Carbon\Carbon::now()->format('Y-m-d') }}</lastmod>
         </sitemap>
     @endforeach    
