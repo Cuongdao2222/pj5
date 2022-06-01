b9b83ada4955abfc3afc89649bc015a6d9b0c7b6
Cuongdao <cuongdao2692@gmail.com>
2022-06-01 08:49:07 +0700

    update code

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -1314,8 +1314,9 @@
                                 <div class="clear"></div>
 
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                     <button type="submit" class="btn btn-primary order1">Đặt hàng</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    
                                 </div>
 
 
