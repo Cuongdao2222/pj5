c23cd14a1ad78f7cb2e5ae390f39d2d00fe66783
Cuongdao <cuongdao2692@gmail.com>
2022-06-02 09:35:35 +0700

    sua code

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -609,14 +609,7 @@
                  
 
                 <?php
-
-                     $minutes = 10;
-
-                    $check = Cache::remember('check',$minutes, function() {
-                        return DB::table('imagecrawl')->select('image')->where('product_id', $data->id)->where('active',0)->get()->pluck('image')->toArray();
-                    });
-
-                   
+                    $check = DB::table('imagecrawl')->select('image')->where('product_id', $data->id)->where('active',0)->get()->pluck('image')->toArray();
 
                      $details = $data->Detail;
                     if(isset($check)){
