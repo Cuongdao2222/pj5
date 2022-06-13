b7f72beaa413e659131158c1de4ae9baaa9ad4aa
Cuongdao <cuongdao2692@gmail.com>
2022-06-13 15:44:34 +0700

    update code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -3,7 +3,7 @@
 @section('content') 
     @push('style')
 
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}?ver=2">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}?ver=3">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/dienmay.css') }}?ver=2"> 
         
         <link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}?ver=2">
