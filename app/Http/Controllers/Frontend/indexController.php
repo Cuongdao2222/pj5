ed3da991309a657035522bde7f4115e2c1a70a10
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-06-20 09:30:15 +0700

    update code

--- app/Http/Controllers/Frontend/indexController.php
+++ app/Http/Controllers/Frontend/indexController.php
@@ -19,7 +19,7 @@ class indexController extends Controller
         $minutes = 10;
 
         $banners = Cache::remember('banners',$minutes, function() {
-            return banners::where('option', 0)->OrderBy('stt', 'asc')->where('active', 1)->get();
+            return banners::where('option', 0)->OrderBy('stt', 'asc')->where('active', 1)->select('title', 'image', 'title', 'link')->get();
         });
 
         $bannersRight = Cache::remember('bannersRights',$minutes, function() {
