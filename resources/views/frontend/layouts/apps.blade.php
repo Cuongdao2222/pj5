9a7d8324924fc7c9a1138408ccecd7207777276e
Cuongdao2222 <cuongdao2692@gmail.com>
2022-07-02 10:17:19 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -1497,7 +1497,7 @@
         </script>
 
 
-        <div style="position:fixed; bottom:10px !important; left:50% !important;transform: translate(0, -50%) !important;" class="zalo-chat-widget" data-oaid="1329456933344915716" data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height=""></div>
+        <div style="position:fixed; bottom:10px !important; left:50% !important;transform: translate(0, 0) !important;" class="zalo-chat-widget" data-oaid="1329456933344915716" data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height=""></div>
 
 
 
