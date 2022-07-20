d4c956d50007566e2d488a4a94942c6ef3f0c7e9
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-07-20 09:28:47 +0700

    update  code

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            'throttle:120,1',
+            'throttle:10,0.016',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
