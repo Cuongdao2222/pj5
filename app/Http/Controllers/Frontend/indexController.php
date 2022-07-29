3b7d9c39536044d226f1c9b54c4a5c714c62b101
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-07-29 15:40:18 +0700

    update code

--- app/Http/Controllers/Frontend/indexController.php
+++ app/Http/Controllers/Frontend/indexController.php
@@ -24,12 +24,13 @@ class indexController extends Controller
 
         $banners =  Cache::get('baners');
 
-        $deal = Cache::get('deals')->orderBy('order', 'desc')->get();
+        $deal_check = deal::OrderBy('end', 'desc')->take(1)->get();
 
-        $deal_check = Cache::get('deals')->orderBy('end', 'desc')->get();
+        $deal = deal::OrderBy('order', 'desc')->get();
 
         $group = Cache::get('groups');
 
+       
         $product_sale = Cache::get('product_sale');
 
          $timeDeal_star = Cache::get('deal_start'); 
@@ -51,7 +52,7 @@ class indexController extends Controller
 
         // }
 
-        if(empty($group) ||empty($banners)||empty($product_sale)){
+        if(empty($group) ||empty($banners)||empty($product_sale)||empty($deal)){
 
             $this->cache();
 
@@ -85,12 +86,15 @@ class indexController extends Controller
         });
 
       
-        return view('frontend.index', compact('banners', 'bannersRight', 'bannerUnderSlider', 'bannerUnderSale','deal','product_sale', 'group','timeDeal_star', 'deal_check'));
+        return view('frontend.index', compact('banners', 'bannersRight', 'bannerUnderSlider', 'bannerUnderSale','deal','product_sale', 'group','timeDeal_star','deal_check'));
     }
     public function cache()
     {
        
+
         $banners = banners::where('option','=',0)->take(6)->OrderBy('stt', 'asc')->where('active','=',1)->select('title', 'image', 'title', 'link')->get();
+
+        $deal_check = deal::OrderBy('end', 'desc')->take(1)->get();
 
         $deal = deal::OrderBy('order', 'desc')->get();
 
@@ -108,6 +112,9 @@ class indexController extends Controller
         Cache::put('product_sale', $product_sale,10000);
         
         Cache::put('baners',$banners,10000);
+
+        Cache::put('deals',$deal,600);
+
     
     }
 
