e81f82523ef449f5e6577001fcf7cd33f104b794
Cuongdao2222 <cuongdao2692@gmail.com>
2022-08-05 18:18:57 +0700

    update code

--- app/Http/Controllers/backend/apiController.php
+++ app/Http/Controllers/backend/apiController.php
@@ -8,24 +8,34 @@ use App\Models\product;
 
 class apiController extends Controller
 {
-    public function updateProduct(Request $request, $id)
+    public function updateProduct(Request $request, $slug)
     {
+        $product = product::where('ProductSku')->first();
 
-       $product = product::find($id);
-        if (empty($product)) {
-            return ['result'=>'id sản phẩm không đúng'];
+        if(!empty($product)&& !empty($product->id)){
+
+            $id = $product->id;
+
+            $product = product::find($id);
+
+            $product->Price =  $request->Price;
+
+            $product->Quantily = $request->Quantily;
+
+            $result = $product->save();
+
+            if($result){
+                return ['result'=>'update thành công'];
+            }
+            else{
+                return ['result'=>'đã xảy ra lỗi trong quá trình update'];
+            }
+
         }
-       $product->Name = $request->Name;
-       $result = $product->save();
+        else{
 
-       if($result){
-            return ['result'=>'update thành công'];
-       }
-       else{
-            return ['result'=>'đã xảy ra lỗi trong quá trình update'];
-       }
-    
-
+            return ['result'=>'model sản phẩm không đúng update thất bại'];
+        }
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 
 Route::get('/user', function () {
     echo "string";
+    die();
 });
 
 
