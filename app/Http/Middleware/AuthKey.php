17bbcfef91d56762c563043af05a74fa8dd01b1b
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-08-05 17:04:52 +0700

    update code

---
+++ app/Http/Middleware/AuthKey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AuthKey
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('APP_KEY');
+        if($token != 'eecc19a1cabb51a5080f6f56399f7e82'){
+            return response()->json(['message'=> 'token not found'], 401);
+        }
+        return $next($request);
+    }
+}
