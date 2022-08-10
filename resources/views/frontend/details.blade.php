0bbe532189e6f8068190c23548f471441b5e2aa7
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-08-10 10:45:06 +0700

    update code

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -448,8 +448,8 @@
                         @else
                         <div class="number-images" style="display: none;">
                             <?php 
-                                $image = App\Models\image::where('product_id', 3751)->select('image')->get();
-                                print_r(trim($data->id));
+                                $image = App\Models\image::where('product_id', trim($data->id))->select('image')->get();
+                                print_r($image[0]->image);
 
                              
                             ?>
