1bd979b08cd9b338087bd852f9ffea643ca89eae
Cuongdao2222 <cuongdao2692@gmail.com>
2022-08-28 23:23:50 +0700

    Update category.blade.php

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -199,7 +199,46 @@
                                         
                                     }
                                 
-                                    
+                                }
+                                else{
+
+                                    // check flash deal
+
+                                    $now = \Carbon\Carbon::createFromDate('28-8-2022, 11:00');
+
+                                    $date_flashdeal = \Carbon\Carbon::create(DB::table('date_flash_deal')->where('id', 1)->first()->date);
+
+                                    if($date_flashdeal->isToday()){
+
+                                        $time1_start = \Carbon\Carbon::createFromDate('28-8-2022, 9:00');
+                                        $time1 = \Carbon\Carbon::createFromDate('28-8-2022, 12:00');
+                                        $time2_start = \Carbon\Carbon::createFromDate('28-8-2022, 12:00');
+                                        $time2 = \Carbon\Carbon::createFromDate('28-8-2022, 14:00');
+                                        $time3_start = \Carbon\Carbon::createFromDate('28-8-2022, 14:00');
+                                        $time3 = \Carbon\Carbon::createFromDate('28-8-2022, 17:00');
+                                        $time4_start = \Carbon\Carbon::createFromDate('28-8-2022, 17:00');
+                                        $time4 = \Carbon\Carbon::createFromDate('28-8-2022, 22:00');
+                                        $define = [['start'=>'9h', 'endTime'=>$time1, 'startTime'=>$time1_start], ['start'=>'12h', 'endTime'=>$time2, 'startTime'=>$time2_start], ['start'=>'14h', 'endTime'=>$time3, 'startTime'=>$time3_start], ['start'=>'17h', 'endTime'=>$time4, 'startTime'=>$time4_start]];
+
+                                        foreach($define as $key => $value)
+
+                                        if($now->between($value['startTime'], $value['endTime'])){
+
+                                            $groups_deal = $key+1;
+
+                                            $flashDeal = App\Models\flashdeal::where('product_id', $id_product)->where('flash_deal_time_id', $groups_deal)->where('active',1)->first();
+
+
+
+                                            if(!empty($flashDeal)){
+
+                                                $price_flash_deal = DB::table('flash_deal')->where('id', $flashDeal->flash_deal_id)->first();
+                                               
+                                            
+                                            }
+
+                                        }
+                                    }
                                 
                                 }
                             ?>
