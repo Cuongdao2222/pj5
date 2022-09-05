74c243fd02f573ada556f959e1f6cbc963b81662
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-09-05 08:38:00 +0700

    update

--- resources/views/frontend/sosanh.blade.php
+++ resources/views/frontend/sosanh.blade.php
@@ -26,73 +26,82 @@
     td{
         text-align: center;
     }
+    @media only screen and (max-width: 767px) {
+     
+        .text-h3{
+            margin-top: 20px !important;
+        }    
+    }
 
 </style>
 <div class="container">
     <h3 class="text-h3">So sánh sản phẩm</h3>
     <br>
-    <table id="tb_padding" border="1" bordercolor="#CCCCCC" style="width:100%">
-        <tbody> 
+    <div class="table-responsive-lg">
+        <table id="tb_padding" border="1" bordercolor="#CCCCCC" style="width:100%">
+            <tbody> 
 
-            <?php
+                <?php
 
-                $filter = [['name'=>'Tên sản phẩm'], ['name'=>'Ảnh'], ['name'=>'Giá bán'],  ['name'=>'Đặc điểm nổi bật']];
+                    $filter = [['name'=>'Tên sản phẩm'], ['name'=>'Ảnh'], ['name'=>'Giá bán'],  ['name'=>'Đặc điểm nổi bật']];
 
-                $_GET['productId'] = 4083;
+                    $_GET['productId'] = 4083;
 
-            ?>
+                ?>
 
-            <?php  
+                <?php  
 
-                $data = $_GET['list'];
+                    $data = $_GET['list'];
 
-                $data = explode(',', $data);
+                    $data = explode(',', $data);
 
-            ?>
-            @if(count($filter)>0 && isset($data))
+                ?>
+                @if(count($filter)>0 && isset($data))
 
-            <?php 
-                $info_product = [];
-                foreach ($data as $key => $value) {
+                <?php 
+                    $info_product = [];
+                    foreach ($data as $key => $value) {
 
-                    $product = App\Models\product::find($value);
+                        $product = App\Models\product::find($value);
 
-                    $info_products = [$product->Name, '<img width="327" src="'.asset($product->Image).'">',  str_replace(',' ,'.', number_format($product->Price)).' đ',  str_replace(array("Đặc điểm nổi bật", " Xem thêm"), '', $product->Salient_Features)];
+                        $info_products = [$product->Name, '<img width="327" src="'.asset($product->Image).'">',  str_replace(',' ,'.', number_format($product->Price)).' đ',  str_replace(array("Đặc điểm nổi bật", " Xem thêm"), '', $product->Salient_Features)];
 
-                    array_push($info_product, $info_products);
-                }
-            ?>
-            @foreach($filter as $key=> $filters)
-            
-            <tr>
-                <td width="120px"><b>{{ $filters['name'] }}</b></td>
+                        array_push($info_product, $info_products);
+                    }
+                ?>
+                @foreach($filter as $key=> $filters)
+                
+                <tr>
+                    <td width="120px"><b>{{ $filters['name'] }}</b></td>
 
-                @foreach($info_product as $info_productss)
+                    @foreach($info_product as $info_productss)
 
-                <td>
-                    <div style="max-height:250px; overflow:auto">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    
-                                    <td class="td-info">
-                                        <span>  
-                                            <label for="code" >{!! $info_productss[$key] !!}</label>
-                                        </span>
-                                    </td>
+                    <td>
+                        <div style="max-height:250px; overflow:auto">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        
+                                        <td class="td-info">
+                                            <span>  
+                                                <label for="code" >{!! $info_productss[$key] !!}</label>
+                                            </span>
+                                        </td>
 
-                                </tr> 
-                            </tbody>
-                        </table>
-                    </div>
-                </td>
+                                    </tr> 
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
+                    @endforeach
+                </tr>
                 @endforeach
-            </tr>
-            @endforeach
-            @endif
-            
-        </tbody>
-    </table>
+                @endif
+                
+            </tbody>
+        </table>
+    </div>
+    
 </div>
 
 
