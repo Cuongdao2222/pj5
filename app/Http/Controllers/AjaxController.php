ede5c1e9f51145d80042d931f2022881229b4886
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-09-21 11:20:15 +0700

    update search

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -244,10 +244,10 @@ class AjaxController extends Controller
 
                         if($product->count()==0){
                             // search bằng thư viện FullTextSearch
-                            $product = product::FullTextSearch('Name', $search)->select('id', 'Name', 'Price', 'Link', 'Image')->get();
+                            $product = product::FullTextSearch('Name', $search)->select('id', 'Name', 'Price', 'Link', 'Image')->where('active',1)->get();
                             
                             if($product->count()==0){
-                                $product = product::where('Name', 'like', '%'.$search.'%')->get();
+                                $product = product::where('Name', 'like', '%'.$search.'%')->where('active',1)->get();
                             }
                         } 
                        
