c97306dfdad41305bb2125d0ba975cd9831071c8
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-06 16:36:54 +0700

    ẩn zalo

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -2241,11 +2241,11 @@
             
         </script>
 
-        <a href="https://zalo.me/0983612828" target="_blank">
+        <!-- <a href="https://zalo.me/0983612828" target="_blank">
             <div style="position: fixed; bottom: 52px; left: 52px; transform: translate(0px, 0px) !important; z-index: 2147483644; border: none; visibility: visible; right: 0px; width: 60px; height: 60px;" class="zalo-chat-widget"data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height="">
                 <img src="https://page.widget.zalo.me/static/images/2.0/Logo.svg">
             </div>
-        </a>
+        </a> -->
 
         <a href="https://zalo.me/0913011888" target="_blank">
             <div style="position: fixed; bottom: 28px !important; left: 52px !important; transform: translate(0px, 0px) !important; z-index: 2147483644; border: none; visibility: visible; right: 0px; width: 60px; height: 60px;" class="zalo-chat-widget"data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height="">
