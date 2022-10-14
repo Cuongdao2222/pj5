10d8660cf0d1a9bcb35301b7ebd807d44e9b692b
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-14 13:37:39 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -933,10 +933,10 @@
             navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
             responsive:{
                 0:{
-                    items:2.5
+                    items:2
                 },
                 600:{
-                    items:2.5
+                    items:2
                 },
                 1000:{
                     items:5
