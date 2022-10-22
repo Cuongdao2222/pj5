729cab00669498964fe4d5aefb2d703c5607ec6b
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-22 16:06:23 +0700

    update css

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -229,7 +229,7 @@
         
         <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}?ver=1"> 
         <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/apps.css') }}?ver=10">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/apps.css') }}?ver=14">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
     
@@ -259,8 +259,12 @@
 
             .category__all{
                 color: #fff !important;
-                font-weight: bold;
+               
             } 
+
+            .list-menu .category{
+                width: 11%;
+            }
 
             .compare-pro-holder a {
                 display: block;
@@ -1747,6 +1751,15 @@
                             <a class="list-mn" href="{{route('details','may-loc-nuoc-ao-smith')}}">
                             <i class="fa-regular fa-refrigerator"></i>
                             <span>Máy lọc nước</span>
+                            </a>
+                            <div class="navmwg accessories danh-muc9" style="display: none;">
+                            </div>
+                        </li>
+
+                        <li class="child" data-id="danh-muc9">
+                            <a class="list-mn" href="{{route('details','may-say-quan-ao')}}">
+                            <i class="fa-regular fa-refrigerator"></i>
+                            <span>Máy sấy quần áo</span>
                             </a>
                             <div class="navmwg accessories danh-muc9" style="display: none;">
                             </div>
