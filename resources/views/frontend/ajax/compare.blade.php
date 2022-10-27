57d3e79f7b01ee7d1d86d7519aa7cdd282d8726d
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-27 10:47:19 +0700

    update

--- resources/views/frontend/ajax/compare.blade.php
+++ resources/views/frontend/ajax/compare.blade.php
@@ -4,7 +4,7 @@
 @foreach($data as $datas)
 
 <a href="javascript:void(0)" class="js-compare-item position-relative" data-id="39839">
-    <span class="remove-compare js-remove-compare" onclick="removeCompare(this)"></span>
+    <!-- <span class="remove-compare js-remove-compare" onclick="removeCompare({{ $datas->id }})">X</span> -->
     <img src="{{ asset($datas->Image) }}">
     <span>{{ $datas->Name }}</span>
 </a>
