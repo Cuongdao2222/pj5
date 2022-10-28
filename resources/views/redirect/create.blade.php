10bd0d79a2b903d24390179d597e2a260f2a04a2
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-28 16:39:31 +0700

    update

--- resources/views/redirect/create.blade.php
+++ resources/views/redirect/create.blade.php
@@ -10,20 +10,29 @@
         <div class="group-fields">
 
             <table id="tb_padding">
-                <tbody><tr>
-                    <td>Link cũ</td>
-                    <td><input type="text" size="80" name="info[request_path]" id="request_path" value="/dieu-hoa-daikin-ftkc71uvmv-1-chieu-inverter-24200btu/"></td>
-                </tr>
-                <tr>
-                    <td>Link đích mới</td>
-                    <td><input type="text" size="80" name="info[target_path]" id="target_path" value="https://dienmaynguoiviet.vn/dieu-hoa-daikin-ftkc71uvmv-1-chieu-inverter-24000btu/"></td>
-                </tr>
-                <tr>
-                    <td>Redirect Code</td>
-                    <td><select name="redirect_code" id="redirect_code"><option>--Chọn--</option><option value="0">No redirect</option><option value="301" selected="">301 - Moved Permanently</option><option value="302">302 - Moved Temporarily</option></select></td>
-                </tr>
+                <tbody>
 
-            </tbody></table>
+                    <form action="post" method="{{ route('create-redirect-link') }}">
+                        @csrf
+                        <tr>
+                            <td>Link cũ</td>
+                            <td><input type="text" size="80" name="request_path" id="request_path" value="" required></td>
+                        </tr>
+                        <tr>
+                            <td>Link đích mới</td>
+                            <td><input type="text" size="80" name="target_path" id="target_path" value="" required></td>
+                        </tr>
+                        <tr>
+                            <td>Redirect Code</td>
+                            <td><select name="redirect_code" id="redirect_code"><option>--Chọn--</option><option value="0">No redirect</option><option value="301" selected="">301 - Moved Permanently</option><option value="302">302 - Moved Temporarily</option></select></td>
+                        </tr>
+
+
+                    </form>
+                    
+
+                </tbody>
+            </table>
 
         </div>
         <div class="group-actions">
