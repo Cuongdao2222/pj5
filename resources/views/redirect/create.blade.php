ea31716280fcb7f6937214147d3aea9d49c2cbd7
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-29 08:30:47 +0700

    update code

--- resources/views/redirect/create.blade.php
+++ resources/views/redirect/create.blade.php
@@ -2,45 +2,39 @@
 
 @section('content')
 
-<form method="post" enctype="multipart/form-data">
+<div class="group">
+    <div class="group-fields">
 
-    <input type="hidden" name="info[id]" value="164654">
+        <table id="tb_padding">
+            <tbody>
 
-    <div class="group">
-        <div class="group-fields">
-
-            <table id="tb_padding">
-                <tbody>
-
-                    <form action="post" method="{{ route('create-redirect-link') }}">
-                        @csrf
-                        <tr>
-                            <td>Link cũ</td>
-                            <td><input type="text" size="80" name="request_path" id="request_path" value="" required></td>
-                        </tr>
-                        <tr>
-                            <td>Link đích mới</td>
-                            <td><input type="text" size="80" name="target_path" id="target_path" value="" required></td>
-                        </tr>
-                        <tr>
-                            <td>Redirect Code</td>
-                            <td><select name="redirect_code" id="redirect_code"><option>--Chọn--</option><option value="0">No redirect</option><option value="301" selected="">301 - Moved Permanently</option><option value="302">302 - Moved Temporarily</option></select></td>
-                        </tr>
+                <form action="post" method="{{ route('create-redirect-link') }}">
+                    @csrf
+                    <tr>
+                        <td>Link cũ</td>
+                        <td><input type="text" size="80" name="request_path" id="request_path" value="" required></td>
+                    </tr>
+                    <tr>
+                        <td>Link đích mới</td>
+                        <td><input type="text" size="80" name="target_path" id="target_path" value="" required></td>
+                    </tr>
+                    <tr>
+                        <td>Redirect Code</td>
+                        <td><select name="redirect_code" id="redirect_code"><option>--Chọn--</option><option value="0">No redirect</option><option value="301" selected="">301 - Moved Permanently</option><option value="302">302 - Moved Temporarily</option></select></td>
+                    </tr>
 
 
-                    </form>
-                    
+                </form>
+                
 
-                </tbody>
-            </table>
+            </tbody>
+        </table>
 
-        </div>
-        <div class="group-actions">
-            <input type="hidden" name="create" value="yes">
-            <input class="btn" type="submit" value="Cập nhật"> hoặc <a href="?opt=url">Hủy bỏ</a>
-        </div>
     </div>
-
-</form>
+    <div class="group-actions">
+        <input type="hidden" name="create" value="yes">
+        <input class="btn" type="submit" value="Cập nhật"> hoặc <a href="?opt=url">Hủy bỏ</a>
+    </div>
+</div>
 
 @endsection
