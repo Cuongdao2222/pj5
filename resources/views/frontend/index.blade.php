779e4d4a4e4386a32cb15bf0ac09c6553658cc57
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-11-04 17:00:57 +0700

    up

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,10 +4,16 @@
     @push('style')
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}?ver=21">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/dienmay.css') }}?ver=22"> 
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/dienmay.css') }}?ver=24"> 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}?ver=3">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/homes.css') }}?ver=8">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/homecs.css') }}?ver=8">
+
+        <style type="text/css">
+           
+        </style>
+
+
     @endpush
 
      <?php
@@ -635,8 +641,8 @@
         @if(!empty($bannerscrollRight) && !empty($bannerscrollLeft))
         <!-- Banner dọc 2 bên -->
         <div class="sticky-sidebar">
-            <a data-cate="0" data-place="1863" href="{{ $bannerscrollLeft->link }}" class="banner-left"><img style="cursor:pointer" src="{{ asset($bannerscrollLeft->image) }}" alt="Theme Giáng Sinh Trái" width="179" height="271"></a>
-            <a data-cate="0" data-place="1864" href="{{ $bannerscrollRight->link }}" class="banner-right"><img style="cursor:pointer" src="{{ asset($bannerscrollRight->image) }}" alt="Theme Giáng Sinh Phải" width="179" height="271"></a>        
+            <a data-cate="0" data-place="1863" href="{{ $bannerscrollLeft->link }}" class="banner-left" id="banner-left-scroll"><img style="cursor:pointer" src="{{ asset($bannerscrollLeft->image) }}" alt="Theme Giáng Sinh Trái" width="179" height="271"></a>
+            <a data-cate="0" data-place="1864" href="{{ $bannerscrollRight->link }}" class="banner-right" id="banner-right-scroll"><img style="cursor:pointer" src="{{ asset($bannerscrollRight->image) }}" alt="Theme Giáng Sinh Phải" width="179" height="271"></a>        
         </div>
         @endif
         <?php  
@@ -747,13 +753,41 @@
 
     @push('script')
 
+    <style type="text/css">
+        
+    </style>
+
+
     <script type="text/javascript">
+
+       
          $('.sticky-sidebar').hide();
         $(window).scroll(function (){
 
             if($(window).scrollTop()>$('.menus-banner').offset().top){
 
+
+                var w = window.innerWidth;
+
+                width = (w - 1200)/2;
+
+                var leftClass = {
+                    left: 'calc((100vw - 1200px)/2) -'+width+'px',
+                   
+                };
+
+                var rightClass = {
+                    right: 'calc((100vw - 1200px)/2) -'+width+'px',
+                   
+                }
+
                 $('.sticky-sidebar').show();
+
+                $('#banner-left-scroll').css(leftClass);
+
+                $('#banner-right-scroll').css(rightClass);
+
+                $('.sticky-sidebar a').css('width', width+'px');
             }
             else{
                 $('.sticky-sidebar').hide();
