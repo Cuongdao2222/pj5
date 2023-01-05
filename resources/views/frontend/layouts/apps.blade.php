1bffd96f53c633b2eab5713df8a32f8c36684cee
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-01-05 14:53:41 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -2329,101 +2329,69 @@
         </footer>
 
         
+        <!-- hoa mai rơi -->
+        <script type='text/javascript'>
+            //<![CDATA[
+            var pictureSrc ="https://1.bp.blogspot.com/-CXx9jt2JMRk/Vq-Lh5fm88I/AAAAAAAASwo/XivooDn_oSY/s1600/hoamai.png"; //the location of the snowflakes
+            var pictureWidth = 15; //the width of the snowflakes
+            var pictureHeight = 15; //the height of the snowflakes
+            var numFlakes = 10; //the number of snowflakes
+            var downSpeed = 0.009; //the falling speed of snowflakes (portion of screen per 100 ms)
+            var lrFlakes = 10; //the speed that the snowflakes should swing from side to side
+            if( typeof( numFlakes ) != 'number' || Math.round( numFlakes ) != numFlakes || numFlakes < 1 ) { numFlakes = 10; }
+            //draw the snowflakes
+            for( var x = 0; x < numFlakes; x++ ) {
+            if( document.layers ) { //releave NS4 bug
+            document.write('<layer id="snFlkDiv'+x+'"><imgsrc="'+pictureSrc+'" height="'+pictureHeight+'"width="'+pictureWidth+'" alt="*" border="0"></layer>');
+            } else {
+            document.write('<div style="position:absolute; z-index:9999;"id="snFlkDiv'+x+'"><img src="'+pictureSrc+'"height="'+pictureHeight+'" width="'+pictureWidth+'" alt="*"border="0"></div>');
+            }
+            }
+            //calculate initial positions (in portions of browser window size)
+            var xcoords = new Array(), ycoords = new Array(), snFlkTemp;
+            for( var x = 0; x < numFlakes; x++ ) {
+            xcoords[x] = ( x + 1 ) / ( numFlakes + 1 );
+            do { snFlkTemp = Math.round( ( numFlakes - 1 ) * Math.random() );
+            } while( typeof( ycoords[snFlkTemp] ) == 'number' );
+            ycoords[snFlkTemp] = x / numFlakes;
+            }
+            //now animate
+            function flakeFall() {
+            if( !getRefToDivNest('snFlkDiv0') ) { return; }
+            var scrWidth = 0, scrHeight = 0, scrollHeight = 0, scrollWidth = 0;
+            //find screen settings for all variations. doing this every time allows for resizing and scrolling
+            if( typeof( window.innerWidth ) == 'number' ) { scrWidth = window.innerWidth; scrHeight = window.innerHeight; } else {
+            if( document.documentElement && (document.documentElement.clientWidth ||document.documentElement.clientHeight ) ) {
+            scrWidth = document.documentElement.clientWidth; scrHeight = document.documentElement.clientHeight; } else {
+            if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            scrWidth = document.body.clientWidth; scrHeight = document.body.clientHeight; } } }
+            if( typeof( window.pageYOffset ) == 'number' ) { scrollHeight = pageYOffset; scrollWidth = pageXOffset; } else {
+            if( document.body && ( document.body.scrollLeft ||document.body.scrollTop ) ) { scrollHeight = document.body.scrollTop;scrollWidth = document.body.scrollLeft; } else {
+            if(document.documentElement && (document.documentElement.scrollLeft ||document.documentElement.scrollTop ) ) { scrollHeight =document.documentElement.scrollTop; scrollWidth =document.documentElement.scrollLeft; } }
+            }
+            //move the snowflakes to their new position
+            for( var x = 0; x < numFlakes; x++ ) {
+            if( ycoords[x] * scrHeight > scrHeight - pictureHeight ) { ycoords[x] = 0; }
+            var divRef = getRefToDivNest('snFlkDiv'+x); if( !divRef ) { return; }
+            if( divRef.style ) { divRef = divRef.style; } var oPix = document.childNodes ? 'px' : 0;
+            divRef.top = ( Math.round( ycoords[x] * scrHeight ) + scrollHeight ) + oPix;
+            divRef.left = ( Math.round( ( ( xcoords[x] * scrWidth ) - (pictureWidth / 2 ) ) + ( ( scrWidth / ( ( numFlakes + 1 ) * 4 ) ) * (Math.sin( lrFlakes * ycoords[x] ) - Math.sin( 3 * lrFlakes * ycoords[x]) ) ) ) + scrollWidth ) + oPix;
+            ycoords[x] += downSpeed;
+            }
+            }
+            //DHTML handlers
+            function getRefToDivNest(divName) {
+            if( document.layers ) { return document.layers[divName]; } //NS4
+            if( document[divName] ) { return document[divName]; } //NS4 also
+            if( document.getElementById ) { return document.getElementById(divName); } //DOM (IE5+, NS6+, Mozilla0.9+, Opera)
+            if( document.all ) { return document.all[divName]; } //Proprietary DOM - IE4
+            return false;
+            }
+            window.setInterval('flakeFall();',150);
+            //]]>
+        </script>
 
-        <div class="snowflakes" aria-hidden="true">
-            <div class="snowflake">❅</div>
-            <div class="snowflake">❆</div>
-            <div class="snowflake">❅</div>
-            <div class="snowflake">❆</div>
-            <div class="snowflake">❅</div>
-            <div class="snowflake">❆</div>
-            <div class="snowflake">❅</div>
-            <div class="snowflake">❆</div>
-            <div class="snowflake">❅</div>
-            <div class="snowflake">❆</div>
-            <div class="snowflake">❅</div>
-            <div class="snowflake">❆</div>
-        </div>
 
-
-<style>
-  @-webkit-keyframes snowflakes-fall {
-    0% {top:-10%}
-    100% {top:100%}
-  }
-  @-webkit-keyframes snowflakes-shake {
-    0%,100% {-webkit-transform:translateX(0);transform:translateX(0)}
-    50% {-webkit-transform:translateX(80px);transform:translateX(80px)}
-  }
-  @keyframes snowflakes-fall {
-    0% {top:-10%}
-    100% {top:100%}
-  }
-  @keyframes snowflakes-shake {
-    0%,100%{ transform:translateX(0)}
-    50% {transform:translateX(80px)}
-  }
-  .snowflake {
-    color: #fff;
-    font-size: 1em;
-    font-family: Arial, sans-serif;
-    text-shadow: 0 0 5px #000;
-    position:fixed;
-    top:-10%;
-    z-index:9999;
-    -webkit-user-select:none;
-    -moz-user-select:none;
-    -ms-user-select:none;
-    user-select:none;
-    cursor:default;
-    -webkit-animation-name:snowflakes-fall,snowflakes-shake;
-    -webkit-animation-duration:10s,3s;
-    -webkit-animation-timing-function:linear,ease-in-out;
-    -webkit-animation-iteration-count:infinite,infinite;
-    -webkit-animation-play-state:running,running;
-    animation-name:snowflakes-fall,snowflakes-shake;
-    animation-duration:10s,3s;
-    animation-timing-function:linear,ease-in-out;
-    animation-iteration-count:infinite,infinite;
-    animation-play-state:running,running;
-  }
-  .snowflake:nth-of-type(0){
-    left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s
-  }
-  .snowflake:nth-of-type(1){
-    left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s
-  }
-  .snowflake:nth-of-type(2){
-    left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s
-  }
-  .snowflake:nth-of-type(3){
-    left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s
-  }
-  .snowflake:nth-of-type(4){
-    left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s
-  }
-  .snowflake:nth-of-type(5){
-    left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s
-  }
-  .snowflake:nth-of-type(6){
-    left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s
-  }
-  .snowflake:nth-of-type(7){
-    left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s
-  }
-  .snowflake:nth-of-type(8){
-    left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s
-  }
-  .snowflake:nth-of-type(9){
-    left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s
-  }
-  .snowflake:nth-of-type(10){
-    left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s
-  }
-  .snowflake:nth-of-type(11){
-    left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s
-  }
-</style>
 
         <div class="hotlines">   
             <a href="tel:02473036336"><img src="{{ asset('images/template/tongdai02473036336.png') }}" alt="hotline" style="height:55px; z-index: 999;"></a>
