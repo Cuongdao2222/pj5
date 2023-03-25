f1e006e833b88838f1995ada9cbf52d0c5eece88
Cuongdao2222 <cuongdao12121@gmail.com>
2023-03-25 15:26:16 +0700

    update

--- resources/views/vendor/adminlte-templates/common/paginate.blade.php
+++ resources/views/vendor/adminlte-templates/common/paginate.blade.php
@@ -1,3 +1,19 @@
 <div class="row">
+
+    <?php 
+
+        $get_find_searchPost = $_GET['searchPost']??'';
+
+    ?>
+    @if(!empty($get_find_searchPost))
+
+        {!! str_replace('find-post?', 'find-post?searchPost='.$get_find_searchPost.'&', $records->links()) !!}
+       
+    @else
+
     {!! $records->links() !!}
+    @endif
+
+
+    
 </div>
