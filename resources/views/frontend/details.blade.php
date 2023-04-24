cc1d98a2b79cad6385f0e9e4f2702c7c1cbe4dc1
Cuongdao2222 <cuongdao12121@gmail.com>
2023-04-24 14:03:52 +0700

    update

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -1747,17 +1747,31 @@
 
 <script type="text/javascript">
 
+    function formatMoney(number, decPlaces, decSep, thouSep) {
+        decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+        decSep = typeof decSep === "undefined" ? "." : decSep;
+        thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+        var sign = number < 0 ? "-" : "";
+        var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
+        var j = (j = i.length) > 3 ? j % 3 : 0;
+
+        return sign +
+            (j ? i.substr(0, j) + thouSep : "") +
+            i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+            (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+    }
+
 
     $('.option-price').change(function(){
 
      
         const price = {{  $value->Price }};
 
-        new_price   =  parseInt(price)+200;
+        new_price   =  parseInt(price) + 100000;
+
+        // price_format = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(new_price);
 
         console.log(price);
-
-        
 
     })
 
