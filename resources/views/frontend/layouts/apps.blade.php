c8165680593cb5bc0a7f4d8ea63b9812e23eeb1f
Cuongdao2222 <cuongdao12121@gmail.com>
2023-05-05 09:32:27 +0700

    update css

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -232,7 +232,7 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}?ver=1"> 
         <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/apps.css') }}?ver=14">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/dienmay.css')}}?ver=95">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/dienmay.css')}}?ver=96">
         <link rel="stylesheet" type="text/css" href="{{asset('css/detailsfe.css')}}?ver=7"> 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -405,7 +405,7 @@
             }
 
             .header__top {
-                background-color: #3bc656 !important;
+                background-color: #3bbcc6 !important;
             }
         </style>
 
