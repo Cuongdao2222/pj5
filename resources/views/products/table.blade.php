4fcc5645f2b5eddc71e37234b2836749b7491de1
Cuongdao2222 <cuongdao12121@gmail.com>
2023-07-10 08:50:38 +0700

    update code

--- resources/views/products/table.blade.php
+++ resources/views/products/table.blade.php
@@ -120,7 +120,7 @@
         @foreach($products as $product)
 
             <tr>
-                <td><img src="{{ asset($product->Image) }}" width="100px" height="150px"></td>
+                <td><img src="{{ asset($product->Image) }}" width="100px"></td>
             <td style="width: 500px;">
                 <a href="{{ route('products.edit', [$product->id]) }}">{{ $product->Name }}</a>
                 <br>
