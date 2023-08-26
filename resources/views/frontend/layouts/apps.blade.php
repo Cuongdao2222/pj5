6182130b0d4c201a59c110743d6aa42cd23b59cb
Cuongdao2222 <cuongdao12121@gmail.com>
2023-08-26 15:43:26 +0700

    update code

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -228,7 +228,7 @@
 
 
         <link rel="stylesheet" href="{{ asset('css/lib/bootstrap.min.css') }}">
-       <link rel="stylesheet" href="{{ asset('css/font-awesome_6.0.0_css_all.min.css') }}"/>
+       <link rel="stylesheet" href="{{ asset('css/fontawesome/css/fontawesome.min.css') }}"/>
         
         <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}?ver=1"> 
         <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
