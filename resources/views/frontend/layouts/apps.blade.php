a15a45e5cf98d1ded04a24b72874e511b97ae192
Cuongdao2222 <cuongdao12121@gmail.com>
2023-08-26 15:46:43 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -228,10 +228,10 @@
 
 
         <link rel="stylesheet" href="{{ asset('css/lib/bootstrap.min.css') }}">
-       <link rel="stylesheet" href="{{ asset('css/fontawesome/css/fontawesome.min.css') }}"/>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
         
         <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}?ver=1"> 
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+        <script src="{{ asset('js/jquery.js') }}"></script>
         <link rel="stylesheet" type="text/css" href="{{ asset('css/apps.css') }}?ver=14">
         <link rel="stylesheet" type="text/css" href="{{asset('css/dienmay.css')}}?ver=99">
         <link rel="stylesheet" type="text/css" href="{{asset('css/detailsfe.css')}}?ver=7"> 
@@ -2650,7 +2650,7 @@
 
     @stack('script')
 
-  <script src="{{ asset('js/jquery.js') }}"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
 
   <script>
 
