c14fa182c28d42e3547e1ccc9ce8ceb69fbf35e5
Cuongdao2222 <cuongdao12121@gmail.com>
2023-09-25 14:39:10 +0700

    Update CustomnHelper.php

--- app/Helpers/CustomnHelper.php
+++ app/Helpers/CustomnHelper.php
@@ -212,7 +212,7 @@ if(!function_exists('pricesPromotion')){
         });
 
         
-        if($id===''||$checkActiveButton===0||$id===3669||$id===3660||$id===439){
+        if($id===''||$checkActiveButton===0||$id===3669||$id===3660||$id===439||$id===4914||$id===1097||$id===947){
 
             $gift_Price = '';
 
