9ffd53136ce05042a93d0276fe46ba45d091317a
Cuongdao2222 <cuongdao2692@gmail.com>
2023-12-13 08:38:40 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -254,7 +254,7 @@
                             @foreach($post_advice as $value)
 
                             <li>
-                                <a href="{{ route('details', $value->link) }}">{{ @_substrs($value->title, 61) }}</a>
+                                <a href="{{ route('details', $value->link) }}">{{ @_substrs($value->title, 60) }}</a>
                             </li>
 
                             @endforeach
@@ -606,7 +606,7 @@
                                                     <i class="fa-solid fa-star"></i>
                                                 </p>
                                                 <div class="line_break">|</div>
-                                                    <div class="reviewCount">{{ $value->Qualtily??0  }} Sản ph</div>
+                                                    <div class="reviewCount">{{ $value->Qualtily??0  }} Sản phẩm</div>
                                                 </div>
                                                 <div class="container-timeline">
                                                 <span class="timeline"><span style="width: 2%"></span></span>
