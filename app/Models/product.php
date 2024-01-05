4f3ebabecd9d680b3a0f1e68c291d85d26d7d0f5
Cuongdao2222 <cuongdao2692@gmail.com>
2024-01-05 09:02:21 +0700

    update code

--- app/Models/product.php
+++ app/Models/product.php
@@ -44,6 +44,7 @@ class product extends Model
             Cache::forget('view-homes');
 
             Cache::forget('ar_skips');
+            Cache::forget('count_pd');
 
             Cache::forget('product_sale');
             $productss = product::select('Link', 'Name', 'Image', 'Price', 'id', 'ProductSku', 'manuPrice', 'orders_hot', 'Salient_Features')->where('active', 1)->get();
@@ -58,6 +59,8 @@ class product extends Model
 
             Cache::forget('view-homes');
 
+            Cache::forget('count_pd');
+
             Cache::forget('ar_skips');
 
             Cache::forget('product_sale');
@@ -69,6 +72,7 @@ class product extends Model
             Cache::forget('product_search');
             Cache::forget('product_sale');
             Cache::forget('view-homes');
+            Cache::forget('count_pd');
 
             Cache::forget('ar_skips');
             $productss = product::select('Link', 'Name', 'Image', 'Price', 'id', 'ProductSku','manuPrice', 'orders_hot', 'Salient_Features')->where('active', 1)->get();
