e5374f62e70bf8edecd2a95dbfff4b071c1d40dd
Cuongdao2222 <cuongdao2692@gmail.com>
2024-04-22 10:09:00 +0700

    update code

--- resources/views/css/fileCss.blade.php
+++ resources/views/css/fileCss.blade.php
@@ -38,7 +38,7 @@
 
         <input type="hidden" name="file" value="{{ $page[$id] }}">  
 
-        <textarea style="width: 800px; height: 1900px;" name="css" class="lined">{!!  $contents  !!}</textarea>
+        <textarea style="width: 800px; height: 1900px;" name="css" class="lined" spellcheck="false">{!!  $contents  !!}</textarea>
 
          <br>
        
