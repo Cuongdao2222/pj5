026f56ab8215420c6d4bf09c622901553cd1785b
Cuongdao2222 <cuongdao2692@gmail.com>
2024-04-22 09:22:12 +0700

    update code

--- resources/views/css/fileCss.blade.php
+++ resources/views/css/fileCss.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
+@push('style')
+<link rel="stylesheet" type="text/css" href="{{ asset('lined-textarea/jquery-linedtextarea.css') }}">
+<script src="{{ asset('lined-textarea/jquery-linedtextarea.js') }}"></script>
+
+ <style type="text/css">
+        
+        .main-header{
+            position: static !important;
+        }
+
+        textarea {
+            outline: none;
+        }
+        textarea {
+            -webkit-box-shadow: none !important;
+            box-shadow: none !important;
+        }
+
+        textarea:focus {
+            outline: none;
+        }
+
+    </style>
+@endpush
+
 @section('content')
     <?php 
         $page = ['homecs.css', 'categorycs.css', 'detailscs.css'];
@@ -13,12 +38,18 @@
 
         <input type="hidden" name="file" value="{{ $page[$id] }}">  
 
-        <textarea style="width: 800px; height: 1900px;" name="css">{{ strip_tags(nl2br($contents))  }}</textarea>
+        <textarea style="width: 800px; height: 1900px;" name="css" class="lined">{!!  $contents  !!}</textarea>
 
          <br>
        
        
     </form>
+
+     <script type="text/javascript">
+        $(function() {
+            $(".lined").linedtextarea();
+        });
+    </script>
     
 @endsection
 
