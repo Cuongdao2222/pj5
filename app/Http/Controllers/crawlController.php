3a995d596cfb14eaa01797a3dbf9ffda41d66c39
Cuongdao2222 <cuongdao2692@gmail.com>
2024-12-20 08:36:02 +0700

    update

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -136,7 +136,7 @@ class crawlController extends Controller
     public function edit_ton_gr()
     {
 
-        $group = groupProduct::find(3);
+        $group = groupProduct::find(4);
 
         $list_pd =  json_decode($group->product_id);
 
