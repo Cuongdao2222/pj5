f5d6203a9106911ece2a6312ef1189254c0c7898
Cuongdao2222 <cuongdao2692@gmail.com>
2025-01-08 08:55:20 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -635,7 +635,7 @@
                                 <div class="item" id="items-{{ $key }}">
                                     <a href="{{ route('details', $value->link) }}">
                                         <div class="img">
-                                            <img width="327" src="{{ asset($value->image) }}"  data-src="{{ asset($value->image) }}" title="{{ $value->name }}" class="lazyload">
+                                            <img width="327" src="{{ asset($value->image) }}"   title="{{ $value->name }}" >
                                         </div>
                                     </a>
                                     <div class="desc desc-deal{{$key}}">
