fd3fe770288b15703a31a6e7f9cbc815c3c490aa
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-01-16 11:00:37 +0700

    update hieu ung tet mobile

--- resources/views/frontend/index_mb.blade.php
+++ resources/views/frontend/index_mb.blade.php
@@ -1273,150 +1273,313 @@
     <!-- End -->
     <!-- Hiệu ứng ... rơi -->
 
-    <?php 
+    <!-- Hiệu ứng ... rơi -->
 
-
-        $event_check = Cache::rememberForever('events-show', function(){
-
-            $event_check = App\Models\events::find(1);
-
-            return $event_check;
-        });
-
-       
-
-    ?>
+    
 
     <style type="text/css">
-        .snowflake {
-            color: #ddd;
-            }
-            @-webkit-keyframes snowflakes-fall {
-            0% {
-            top: -10%;
-            }
-            100% {
-            top: 100%;
-            }
-            }
-            @-webkit-keyframes snowflakes-shake {
-            0% {
-            -webkit-transform: translateX(0px);
-            transform: translateX(0px);
-            }
-            50% {
-            -webkit-transform: translateX(80px);
-            transform: translateX(80px);
-            }
-            100% {
-            -webkit-transform: translateX(0px);
-            transform: translateX(0px);
-            }
-            }
-            @keyframes snowflakes-fall {
-            0% {
-            top: -10%;
-            }
-            100% {
-            top: 100%;
-            }
-            }
-            @keyframes snowflakes-shake {
-            0% {
-            transform: translateX(0px);
-            }
-            50% {
-            transform: translateX(80px);
-            }
-            100% {
-            transform: translateX(0px);
-            }
-            }
-            .snowflake {
-            position: fixed;
-            top: -10%;
-            z-index: 9999;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-            cursor: default;
-            -webkit-animation-name: snowflakes-fall, snowflakes-shake;
-            -webkit-animation-duration: 10s, 3s;
-            -webkit-animation-timing-function: linear, ease-in-out;
-            -webkit-animation-iteration-count: infinite, infinite;
-            -webkit-animation-play-state: running, running;
-            animation-name: snowflakes-fall, snowflakes-shake;
-            animation-duration: 10s, 3s;
-            animation-timing-function: linear, ease-in-out;
-            animation-iteration-count: infinite, infinite;
-            animation-play-state: running, running;
-            }
-            .snowflake:nth-of-type(0) {
-            left: 1%;
-            -webkit-animation-delay: 0s, 0s;
-            animation-delay: 0s, 0s;
-            }
-            .snowflake:nth-of-type(1) {
-            left: 10%;
-            -webkit-animation-delay: 1s, 1s;
-            animation-delay: 1s, 1s;
-            }
-            .snowflake:nth-of-type(2) {
-            left: 20%;
-            -webkit-animation-delay: 6s, 0.5s;
-            animation-delay: 6s, 0.5s;
-            }
-            .snowflake:nth-of-type(3) {
-            left: 30%;
-            -webkit-animation-delay: 4s, 2s;
-            animation-delay: 4s, 2s;
-            }
-            .snowflake:nth-of-type(4) {
-            left: 40%;
-            -webkit-animation-delay: 2s, 2s;
-            animation-delay: 2s, 2s;
-            }
-            .snowflake:nth-of-type(5) {
-            left: 50%;
-            -webkit-animation-delay: 8s, 3s;
-            animation-delay: 8s, 3s;
-            }
-            .snowflake:nth-of-type(6) {
-            left: 60%;
-            -webkit-animation-delay: 6s, 2s;
-            animation-delay: 6s, 2s;
-            }
-            .snowflake:nth-of-type(7) {
-            left: 70%;
-            -webkit-animation-delay: 2.5s, 1s;
-            animation-delay: 2.5s, 1s;
-            }
-            .snowflake:nth-of-type(8) {
-            left: 80%;
-            -webkit-animation-delay: 1s, 0s;
-            animation-delay: 1s, 0s;
-            }
-            .snowflake:nth-of-type(9) {
-            left: 90%;
-            -webkit-animation-delay: 3s, 1.5s;
-            animation-delay: 3s, 1.5s;
-            }
+        @-webkit-keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+        -ms-transform: rotate(0deg);
+        transform: rotate(0deg)
+    }
+    100% {
+        -webkit-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+        transform: rotate(360deg)
+    }
+}
+
+@keyframes spin {
+    0% {
+        -webkit-transform: rotate(0deg);
+        -ms-transform: rotate(0deg);
+        transform: rotate(0deg)
+    }
+    100% {
+        -webkit-transform: rotate(360deg);
+        -ms-transform: rotate(360deg);
+        transform: rotate(360deg)
+    }
+}
+
+@-webkit-keyframes sonar {
+    0% {
+        opacity: 1;
+        -webkit-transform: scale(1);
+        transform: scale(1)
+    }
+    100% {
+        -webkit-transform: scale(4);
+        transform: scale(4);
+        opacity: 0
+    }
+}
+
+@keyframes sonar {
+    0% {
+        opacity: 1;
+        -webkit-transform: scale(1);
+        transform: scale(1)
+    }
+    100% {
+        -webkit-transform: scale(4);
+        transform: scale(4);
+        opacity: 0
+    }
+}
+
+@keyframes load-effect {
+    0% {
+        transform: rotate(0deg)
+    }
+    100% {
+        transform: rotate(360deg)
+    }
+}
+
+.falling-container {
+    display: none
+}
+
+.theme-lunar-new-year .falling-container {
+    display: block
+}
+
+.falling-item {
+    color: #fff;
+    font-size: 1em;
+    font-family: Arial
+}
+
+.falling-item {
+    position: fixed;
+    top: -10%;
+    z-index: 9999;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    cursor: default;
+    -webkit-animation-name: snowflakes-fall, snowflakes-shake;
+    -webkit-animation-duration: 10s, 3s;
+    -webkit-animation-timing-function: linear, ease-in-out;
+    -webkit-animation-iteration-count: infinite, infinite;
+    -webkit-animation-play-state: running, running;
+    animation-name: snowflakes-fall, snowflakes-shake;
+    animation-duration: 10s, 3s;
+    animation-timing-function: linear, ease-in-out;
+    animation-iteration-count: infinite, infinite;
+    animation-play-state: running, running
+}
+
+.falling-item:nth-of-type(0) {
+    left: 1%;
+    -webkit-animation-delay: 0s, 0s;
+    animation-delay: 0s, 0s
+}
+
+.falling-item:nth-of-type(1) {
+    left: 10%;
+    -webkit-animation-delay: 1s, 1s;
+    animation-delay: 1s, 1s
+}
+
+.falling-item:nth-of-type(2) {
+    left: 20%;
+    -webkit-animation-delay: 6s, .5s;
+    animation-delay: 6s, .5s
+}
+
+.falling-item:nth-of-type(3) {
+    left: 30%;
+    -webkit-animation-delay: 4s, 2s;
+    animation-delay: 4s, 2s
+}
+
+.falling-item:nth-of-type(4) {
+    left: 40%;
+    -webkit-animation-delay: 2s, 2s;
+    animation-delay: 2s, 2s
+}
+
+.falling-item:nth-of-type(5) {
+    left: 50%;
+    -webkit-animation-delay: 8s, 3s;
+    animation-delay: 8s, 3s
+}
+
+.falling-item:nth-of-type(6) {
+    left: 60%;
+    -webkit-animation-delay: 6s, 2s;
+    animation-delay: 6s, 2s
+}
+
+.falling-item:nth-of-type(7) {
+    left: 70%;
+    -webkit-animation-delay: 2.5s, 1s;
+    animation-delay: 2.5s, 1s
+}
+
+.falling-item:nth-of-type(8) {
+    left: 80%;
+    -webkit-animation-delay: 1s, 0s;
+    animation-delay: 1s, 0s
+}
+
+.falling-item:nth-of-type(9) {
+    left: 90%;
+    -webkit-animation-delay: 3s, 1.5s;
+    animation-delay: 3s, 1.5s
+}
+
+@-webkit-keyframes snowflakes-fall {
+    0% {
+        top: -10%
+    }
+    100% {
+        top: 100%
+    }
+}
+
+@-webkit-keyframes snowflakes-shake {
+    0% {
+        -webkit-transform: translateX(0);
+        transform: translateX(0)
+    }
+    50% {
+        -webkit-transform: translateX(80px);
+        transform: translateX(80px)
+    }
+    100% {
+        -webkit-transform: translateX(0);
+        transform: translateX(0)
+    }
+}
+
+@keyframes snowflakes-fall {
+    0% {
+        top: -10%
+    }
+    100% {
+        top: 100%
+    }
+}
+
+@keyframes snowflakes-shake {
+    0% {
+        transform: translateX(0)
+    }
+    50% {
+        transform: translateX(80px)
+    }
+    100% {
+        transform: translateX(0)
+    }
+}
+
+.falling-item {
+    color: transparent;
+    background-size: 100%;
+    color: transparent;
+    background-repeat: no-repeat
+}
+
+.falling-item:nth-of-type(1) {
+    left: 10%;
+    -webkit-animation-delay: 6s, .5s;
+    animation-delay: 6s, .5s;
+    font-size: 2.5em;
+    background-image: url({{ asset('images/posts/tet') }}/pink_1-min.png)
+}
+
+.falling-item:nth-of-type(2) {
+    left: 20%;
+    -webkit-animation-delay: 5s, 5.5s;
+    animation-delay: 5s, 5.5s;
+    font-size: 1.9em;
+    background-image: url({{ asset('images/posts/tet') }}/yellow_2-min.png)
+}
+
+.falling-item:nth-of-type(3) {
+    left: 30%;
+    -webkit-animation-delay: 4s, 2s;
+    animation-delay: 4s, 2s;
+    font-size: 2.4em;
+    background-image: url({{ asset('images/posts/tet') }}/pink_3-min.png)
+}
+
+.falling-item:nth-of-type(4) {
+    left: 40%;
+    -webkit-animation-delay: 3s, 1.5s;
+    animation-delay: 3s, 1.5s;
+    font-size: 2.1em;
+    background-image: url({{ asset('images/posts/tet') }}/yellow_4-min.png)
+}
+
+.falling-item:nth-of-type(5) {
+    left: 50%;
+    -webkit-animation-delay: 2s, 2s;
+    animation-delay: 2s, 2s;
+    font-size: 2.3em;
+    background-image: url({{ asset('images/posts/tet') }}/pink_5-min.png)
+}
+
+.falling-item:nth-of-type(6) {
+    left: 60%;
+    -webkit-animation-delay: 1s, 0s;
+    animation-delay: 1s, 0s;
+    font-size: 2.2em;
+    background-image: url({{ asset('images/posts/tet') }}/yellow_6-min.png)
+}
+.falling-item:nth-of-type(7) {
+    left: 70%;
+    -webkit-animation-delay: 1s, 0s;
+    animation-delay: 1s, 0s;
+    font-size: 2.2em;
+    background-image: url({{ asset('images/posts/tet') }}/yellow_5-min.png)
+}
+
+.falling-item:nth-of-type(8) {
+    left: 80%;
+    -webkit-animation-delay: 1s, 0s;
+    animation-delay: 1s, 0s;
+    font-size: 2.2em;
+    background-image: url({{ asset('images/posts/tet') }}/yellow_4-min.png)
+}
+
     </style>
 
+     <div class="falling-container" aria-hidden="true">
+        <div class="falling-item">
+            ●
+        </div>
+        <div class="falling-item">
+            ●
+        </div>
+        <div class="falling-item">
+            ●
+        </div>
+        <div class="falling-item">
+            ●
+        </div>
 
-    <div class="snowflakes" aria-hidden="true">
-        <div class="snowflake" style="font-size: 30px;">❅</div>
-        <div class="snowflake">❅</div>
-        <div class="snowflake" style="font-size: 40px;">❆ </div>
-        <div class="snowflake">❅</div>
-        <div class="snowflake" style="font-size: 30px;">❆</div>
-        <div class="snowflake" style="font-size: 22px;">❅</div>
-        <div class="snowflake" style="font-size: 50px;">❆</div>
-        <div class="snowflake" style="font-size: 20px;">❅</div>
-        <div class="snowflake" style="font-size: 70px;">❆</div>
-        <div class="snowflake" style="font-size: 20px;">❆</div>
+        <div class="falling-item">
+            ●
+        </div>
+        <div class="falling-item">
+            ●
+        </div>
+        <div class="falling-item">
+            ●
+        </div>
+        <div class="falling-item">
+            ●
+        </div>
+       
     </div>
+
     <!-- End -->
 
     <div class="pine-tree"> 
