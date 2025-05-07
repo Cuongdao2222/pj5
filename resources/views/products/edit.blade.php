3b43c3df1363fd3223d055a1a8b17d38ea76b39b
cuong12121 <cuongdao12121@gmail.com>
2025-05-07 14:36:12 +0700

    update

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -170,7 +170,7 @@
                     $product->Detail = $content1;
                     $product->Detail = str_replace(['http://dienmaynguoiviet.net', 'https://dienmaynguoiviet.net'], 'https://dienmaynguoiviet.vn', $product->Detail);
                     // thay thẻ strong cho đỡ lỗi font chữ 
-                    $product->Detail = preg_replace('/<strong>(.*?)<\/strong>/', '<span style="font-weight: bold">$1</span>', $product->Detail);
+                    $product->Detail = preg_replace('/<strong>(.*?)<\/strong>/', '<span>$1</span>', $product->Detail);
 
                     
                   
