400acd07ae4444f8b4397d59c0ec4ea9bc5ee3d2
cuong12121 <cuongdao12121@gmail.com>
2025-07-23 14:55:06 +0700

    update

--- resources/views/posts/fields.blade.php
+++ resources/views/posts/fields.blade.php
@@ -77,33 +77,35 @@
 ?>
 <br>
 <!-- Title Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('title', 'Title:') !!}
-    {!! Form::text('title', null, ['class' => 'form-control']) !!}
-    
-</div>
+<div class="form-group col-sm-12 col-lg-12">
+    <div class="form-group col-sm-6">
+        {!! Form::label('title', 'Title:') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+        
+    </div>
 
-<?php 
-    if(Schema::hasTable('categories')){
-        $category =    DB::table('categories')->select('namecategory', 'id')->get();
-        $new_category = [];
-        if(isset($category)){
-            foreach ($category as  $value) {
-               $new_category[$value->id] = $value->namecategory;
+    <?php 
+        if(Schema::hasTable('categories')){
+            $category =    DB::table('categories')->select('namecategory', 'id')->get();
+            $new_category = [];
+            if(isset($category)){
+                foreach ($category as  $value) {
+                   $new_category[$value->id] = $value->namecategory;
+                }
             }
+            
+            $categoryselected = !empty($post)?$post['category']:'1';
         }
-        
-        $categoryselected = !empty($post)?$post['category']:'1';
-    }
-     
-        
-?>
-@if(Schema::hasTable('categories'))
-<div class="form-group col-sm-6">
-    {!! Form::label('category', 'category:') !!}
-   {{ Form::select('category', @$new_category, $categoryselected, ['id' => 'category', 'class' => 'form-control']) }}
-</div>
-@endif
+         
+            
+    ?>
+    @if(Schema::hasTable('categories'))
+    <div class="form-group col-sm-6">
+        {!! Form::label('category', 'category:') !!}
+       {{ Form::select('category', @$new_category, $categoryselected, ['id' => 'category', 'class' => 'form-control']) }}
+    </div>
+    @endif
+</div>    
 
 <!-- shortcontent Field -->
 <div class="form-group col-sm-12 col-lg-12">
