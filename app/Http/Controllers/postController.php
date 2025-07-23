71a0f3a35910978ecb82c49d2060c15dd8a63ed5
cuong12121 <cuongdao12121@gmail.com>
2025-07-23 10:14:46 +0700

    update

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -286,16 +286,16 @@ class postController extends AppBaseController
 
         Flash::success('Post updated successfully.');
 
-        // if(!empty($input('datetime') )){
+        if(!empty($input('datetime') )){
 
-        //     dd($input('datetime'));
+            dd($input('datetime'));
 
-        //     $datetime = $input('datetime');
+            // $datetime = $input('datetime');
 
-        //     $expireAt = Carbon::parse($datetime); 
+            // $expireAt = Carbon::parse($datetime); 
 
-        //     Cache::put('set_time_post_'.$id, $datetime, $expireAt);
-        // }   
+            // Cache::put('set_time_post_'.$id, $datetime, $expireAt);
+        }   
 
         if($input['category'] == 5){
 
