fd52688bb4928830c0701ceb862d104323aa2114
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-08-28 11:27:26 +0700

    update tcl

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -147,7 +147,7 @@ class crawlController extends Controller
             array_push($break, $value->id);
         }
 
-        $group = groupProduct::find(13);
+        $group = groupProduct::find(15);
 
         $list_pd =  json_decode($group->product_id);
 
