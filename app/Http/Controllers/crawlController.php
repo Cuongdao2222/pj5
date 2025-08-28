3c8a568c61d5cbcd30c07a488cc1128868cd48b8
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-08-28 10:12:44 +0700

    update

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -147,7 +147,7 @@ class crawlController extends Controller
             array_push($break, $value->id);
         }
 
-        $group = groupProduct::find(3);
+        $group = groupProduct::find(4);
 
         $list_pd =  json_decode($group->product_id);
 
