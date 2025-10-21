d2e52f4f5cc9da014002e9826e98e994baf9783a
cuong12121 <cuongdao12121@gmail.com>
2025-10-21 08:29:22 +0700

    update

--- resources/views/products/fields.blade.php
+++ resources/views/products/fields.blade.php
@@ -129,6 +129,7 @@
                 ?> 
                 <td class="img1{{ $key }}"><a href="javascript:void(0);" onclick="click1('images1{{ $key }}', '{{ asset($images) }}')">
                     <img src="{{ asset($images) }}" style="max-width:100px; max-height:130px" id="img1{{ $key }}"></a>
+                    <br>
                     <a href="{{ route('removeImageContent', $values->id) }}">Xóa ảnh</a>
 
                 </td>
