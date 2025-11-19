1ddcd5cbe59385daeba9bfda97672da03f38dc73
cuong12121 <cuongdao12121@gmail.com>
2025-11-19 09:24:29 +0700

    update

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -847,7 +847,7 @@ class productController extends AppBaseController
 
                         if($product->count()==0){
                             // search bằng thư viện FullTextSearch
-                            $product = product::FullTextSearch('Name', $search)->select('id', 'Name', 'Price', 'Link', 'Image')->where('active', 1)->get();
+                            $product = product::FullTextSearch('Name', $search)->select('id', 'Name', 'Price', 'Link', 'Image')->where('active', 1)->orderBy('Quantily', 'desc')->get();
                             
                             if($product->count()==0){
                                 $product = product::where('Name', 'like', '%'.$search.'%')->where('active', 1)->get();
