2fe3a1b6bf2be5f56f4da2eff44c4b974f8434e9
cuong12121 <cuongdao12121@gmail.com>
2026-01-07 09:43:59 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1693,10 +1693,10 @@
     <div class="pine-tree">
         <img class="pine-tree-left " src="{{ asset('public/background/mai-tree.png')}}" data-was-processed="true"> 
         <img class="pine-tree-right " src="{{ asset('public/background/dao-tree.png')}}" data-was-processed="true">  
-        <img class="tuyet-left loading" src="{{ asset('background/mai-tree.png')}}" data-was-processed="true">  
+       <!--  <img class="tuyet-left loading" src="{{ asset('background/mai-tree.png')}}" data-was-processed="true">  
         <img class="tuyet-right loading" src="{{ asset('background/mai-tree.png')}}" data-was-processed="true">  
         <img class="santa-left loading" src="{{ asset('background/dao-tree.png')}}" data-was-processed="true">  
-        <img class="santa-right loading" src="{{ asset('background/dao-tree.png')}}" data-was-processed="true">
+        <img class="santa-right loading" src="{{ asset('background/dao-tree.png')}}" data-was-processed="true"> -->
     </div> 
 <!-- end hiệu ứng với cả cây thông -->
 
