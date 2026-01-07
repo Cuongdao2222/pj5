3310db165e2764c070133866e1a0daf65ab41547
cuong12121 <cuongdao12121@gmail.com>
2026-01-07 10:36:20 +0700

    update

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -275,7 +275,7 @@
                 position: absolute;
                 width: 100%;
                 top: 0;
-                left: 0;
+                right: 0;
             }
 
             .pine-header-left img{
