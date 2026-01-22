0e5c64818e6b95528630234216d590e46db979fd
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2026-01-22 09:39:28 +0700

    update

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -137,7 +137,7 @@
 
             if(!empty($ar_list[1]['name'])){
 
-                $convert = ['Thương hiệu'=>'Hãng Sản Xuất','Thương Hiệu'=>'Hãng Sản Xuất', 'Kích cỡ tivi'=>'Kích Thước', 'Loại tivi'=>'Loại Tivi', 'Kiểu giặt'=>'Loại Máy Giặt', 'Khối lượng giặt'=>'Khối lượng giặt', 'Dung tích' => 'Dung tích', 'Loại tủ'=>'Kiểu tủ','Công suất'=>'Công suất làm lạnh'];
+                $convert = ['Thương hiệu'=>'Hãng Sản Xuất','Thương Hiệu '=>'Hãng Sản Xuất', 'Kích cỡ tivi'=>'Kích Thước', 'Loại tivi'=>'Loại Tivi', 'Kiểu giặt'=>'Loại Máy Giặt', 'Khối lượng giặt'=>'Khối lượng giặt', 'Dung tích' => 'Dung tích', 'Loại tủ'=>'Kiểu tủ','Công suất'=>'Công suất làm lạnh'];
 
                 $filtername = $convert[$ar_list[1]['name']]??'';
 
