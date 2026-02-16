8f7960d8c38efee201d5f04cedb481f808ac1e24
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2026-02-16 10:22:26 +0700

    update

--- resources/views/frontend/deallist.blade.php
+++ resources/views/frontend/deallist.blade.php
@@ -87,13 +87,18 @@
 
             $check = !empty($_GET['check'])??'';
 
-            if(!empty($check)){
-                dd($deal);
-            }
+
         ?>
         
         @if(!empty($deal)&count($deal)>0)
         <div class="row list-pro">
+
+            <?php 
+
+                if(!empty($check)){
+                    dd(count($deal));
+                }
+            ?>
             
             @foreach($deal as $key => $value)
 
